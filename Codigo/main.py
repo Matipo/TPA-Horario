1fa16c9987f2cab54416dc3c878b560fa1ed2ff9
ElMatias <89108272+Matipo@users.noreply.github.com>
2024-04-02 22:48:11 -0300

    Reworkeo al codigo

--- Codigo/main.py
+++ Codigo/main.py
@@ -42,78 +42,135 @@ elif dia == "Sunday":
 def main(page: ft.Page):
     #Nombre de ventana
     page.title = "TPA Horario"
-    # Definir los días de la semana y las actividades diarias
-    dias = ["","Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
-    
+    #Días de la semana
+    dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
+    page.update()
     
     #Crear item
-    def semanainfo(semanainfo):
-        semanainfo = []
-        semanainfo.append(
-            ft.Container(
-                        content=ft.Text(f"Semana {dia1} - {dia2} {mes} {year}",color=ft.colors.WHITE),
+
+    semanainf = [
+        ft.Container(
+            content=ft.Text(f"Semana {dia1} - {dia2} {mes} {year}",color=ft.colors.WHITE),
                         alignment=ft.alignment.center,
                         width=300,
                         height=30,
                         bgcolor=ft.colors.DEEP_ORANGE_300,
                         border_radius=ft.border_radius.all(5),
-            )  
         )
-        return semanainfo
-    #Para mostrar la fila
-    row1 = ft.Row(controls=semanainfo(0), alignment=ft.MainAxisAlignment.CENTER)
-    page.add(row1)
-    #Crea los cuadros para los dias de la semana
-    def items(i):
-        items = []
+    ]
+    rowinf = ft.Row(controls=[*semanainf],alignment=ft.MainAxisAlignment.CENTER)
+  
+    #Crea los container para los dias de la semana
+    rowz = [
+        ft.Container(
+           content=ft.Text(i,color=ft.colors.WHITE),
+            alignment=ft.alignment.center,
+            width=100,
+            height=50,
+            bgcolor=ft.colors.BLUE_900,
+            border_radius=ft.border_radius.all(5), 
+        )for i in dias
+    ]
+
+    #-------------------- Boton ADD EVENT ----------------------
+    #La funcion de cerrar tambien comprueba si la checkbox esta marcada y la devuelve a su estado de desmarcada
+    def closesave_dlg(e):
+
+        print(user_input.value)
+
+        user_input.value = ""
+
+        if c1.value == True:
+            print("Lunes")
+        if c2.value == True:
+            print("Martes")
+        if c3.value == True:
+            print("Miercoles")
+        if c4.value == True:
+            print("Jueves")
+        if c5.value == True:
+            print("Viernes")
+        if c6.value == True:
+            print("Sabado")
+        if c7.value == True:
+            print("Domingo")
+    
+        c1.value = False
+        c2.value = False
+        c3.value = False
+        c4.value = False
+        c5.value = False
+        c6.value = False
+        c7.value = False
+
+        dlg_modal.open = False
+        page.update()
+    
+    def close_dlg(e):
+        #Desmarca checkboxes
+        c1.value = False
+        c2.value = False
+        c3.value = False
+        c4.value = False
+        c5.value = False
+        c6.value = False
+        c7.value = False
+
+        dlg_modal.open = False
+        page.update()
+
+    #El input para ingresar nombre
+    user_input = ft.TextField(label="Ingresa nombre de evento",autofocus=True)
+
+    #Checkboxes
+    c1 = ft.Checkbox(label=dias[0],value=False)
+    c2 = ft.Checkbox(label=dias[1],value=False)
+    c3 = ft.Checkbox(label=dias[2],value=False)
+    c4 = ft.Checkbox(label=dias[3],value=False)
+    c5 = ft.Checkbox(label=dias[4],value=False)
+    c6 = ft.Checkbox(label=dias[5],value=False)
+    c7 = ft.Checkbox(label=dias[6],value=False)
+
+    #Propiedades de la ventana emergente y lo que muestra
+    dlg_modal = ft.AlertDialog(
+        modal=True,
+
+        title=ft.Text("Add Element"),
+        content=ft.Column(controls=[
+            user_input,
+            c1,c2,c3,c4,c5,c6,c7
+        ]),
+        actions=[
+            ft.TextButton("Save",on_click=closesave_dlg,),
+            ft.TextButton("Cancel", on_click=close_dlg),
+        ],
         
-        for i in dias:
-            
-            if i=="":
-                items.append(
-                    #Crea el boton de Create event
-                    ft.Container(
-                        content=ft.Text("Add element",color=ft.colors.WHITE),
-                        alignment=ft.alignment.center,
-                        width=100,
-                        height=50,
-                        bgcolor=ft.colors.DEEP_ORANGE_300,
-                        border_radius=ft.border_radius.all(20),
-                        ink=True,
-                        on_click=lambda e: print("Boton clickeado"),
-                    )    )
-            else:
-                if i == dia:
-                    #CREA UN CUADRADO DE DISTINTO COLOR SI ES EL DIA EN EL QUE TE ENCUENTRAS
-                    items.append(
-                    ft.Container(
-                    
-                        content=ft.Text(i,color=ft.colors.WHITE),
-                        alignment=ft.alignment.center,
-                        width=100,
-                        height=50,
-                        bgcolor=ft.colors.DEEP_ORANGE_300,
-                        border_radius=ft.border_radius.all(5),
-                    ))
-                else:
-                    items.append(
-                    ft.Container(
-                    
-                        content=ft.Text(i,color=ft.colors.WHITE),
-                        alignment=ft.alignment.center,
-                        width=100,
-                        height=50,
-                        bgcolor=ft.colors.BLUE_900,
-                        border_radius=ft.border_radius.all(5),
-                    ))
-                
-                    
-                    
-        return items
-    row = ft.Row(spacing=10, controls=items(10))
-    page.add(row)
+        actions_alignment=ft.MainAxisAlignment.END,
+    )
     
-    
+    def open_dlg_modal(e):
+        page.dialog = dlg_modal
+        dlg_modal.open = True
+        page.update()
+
+
+    row_dias = ft.Row(
+        controls=[
+            ft.ElevatedButton(
+                "Evento", 
+                on_click=open_dlg_modal,
+                bgcolor=ft.colors.DEEP_ORANGE_300,
+                width=117,
+                height=40,
+                icon="add_circle"
+                ),
+           *rowz
+        ],
+        alignment="center",
+        spacing=10,
+    )
+
+    page.add(rowinf,row_dias)
 
     # Actualizar la página para mostrar los cambios
     page.update()
